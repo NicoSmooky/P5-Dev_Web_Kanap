bd8f8c2077ffe72d64cfeba051f59f31b8c484bf
NicoSmooky <92526241+NicoSmooky@users.noreply.github.com>
2022-07-27 15:55:43 +0200

    Initial commit

---
+++ front/js/index.js
@@ -0,0 +1,58 @@
+let url = "http://localhost:3000/api/products"
+
+fetch(url)
+.then(data=> {
+    return data.json()
+}).then(products=>{
+    console.log(products)
+
+
+    let HTML = document.getElementById("items")
+
+    let myHTML =""
+
+    products.forEach((product) => {
+       
+        console.log(product)
+        console.log(product.name)
+        // console.log(product.id)
+
+        let card = document.createElement ("a");
+        card.href = `./product.html?id=${product._id}`
+        let cardArticle = document.createElement ("article");
+        let cardImg = document.createElement("img");
+        let productName = document.createElement("h3");
+        // productName = document.createTextNode (`${product.name}`)
+        productName.textContent = product.name;
+        let productParagraphe = document.createElement("br");
+        let productDescription = document.createElement("p");
+        cardImg.src = product.imageUrl;
+        // productDescription = document.createTextNode (`${product.description}`)
+        productDescription.textContent = product.description;
+        cardImg.alt = product.altTxt;
+        productName.h3 = product.name;
+        productDescription.p = product.description;
+        cardArticle.appendChild(cardImg)
+        card.appendChild(cardArticle)
+        cardArticle.appendChild(productName)
+        cardArticle.appendChild(productParagraphe)
+        cardArticle.appendChild(productDescription)
+
+        HTML.appendChild(card)
+
+        // myHTML += `<a href="./product.html?id=${product._id}">
+        //             <article>
+        //                 <img src="${product.imageUrl}" alt="${product.name}">
+        //                 <h3 class="productName">${product.name}</h3>
+        //                 <p class="productDescription">${product.description}</p>
+        //             </article>
+        //             </a>`
+
+    
+    });
+    
+    // HTML.innerHTML = myHTML
+    // console.log(myHtml)
+  
+})
+
---
+++ front/js/product.js
@@ -0,0 +1,103 @@
+
+// console.log(window)
+// console.log(window.location)
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+console.log(id)
+
+// let urlProduct = "http://localhost:3000/api/products?id=${product._id}"
+let urlBase = "http://localhost:3000/api/products/"
+let urlProduct = `http://localhost:3000/api/products/${id}`
+// let urlProduct = urlBase + id
+console.log(urlProduct)
+
+
+fetch(urlProduct)
+.then(data=> {
+    return data.json()
+}).then(urlProduct=>{
+    console.log(urlProduct)
+
+    let image = document.querySelector(".item__img img")
+    image.alt = urlProduct.altTxt;
+    image.src = urlProduct.imageUrl;
+
+    
+    let nom = document.getElementById("title")
+    console.log(nom)
+    nom.textContent = urlProduct.name;
+
+    let prix = document.getElementById("price")
+    console.log(price)
+    // console.log(new Intl.NumberFormat().format(prix));
+    // console.log(new Intl.NumberFormat("en-IN", {maximumSignificantDigits: 3}).format(prix));
+    prix.textContent = urlProduct.price;
+
+    let description = document.getElementById("description")
+    console.log(description , image)
+
+    description.textContent = urlProduct.description;
+    
+
+    let couleur = document.getElementById("colors")
+    console.log(colors)
+
+
+    console.log(urlProduct.colors)
+    urlProduct.colors.forEach((color) => {
+        console.log(color)
+
+        let colorElt = document.createElement ("option");
+        //Value
+        colorElt.value = color
+        //Textcontent
+        colorElt.textContent = color
+        colors.appendChild(colorElt)
+
+
+    });
+
+    let btnpanier = document.getElementById("addToCart");
+    console.log(btnpanier)
+    
+    btnpanier.addEventListener("click", event => {
+    event.preventDefault()
+    alert("ok")
+
+
+
+
+    console.log(myProduct)
+
+
+
+
+});
+
+
+
+
+// myProduct = JSON.parse(localStorage.getItem('panier'))
+
+// var myProduct = urlProduct;
+myProduct = {
+    id : urlProduct._id,
+    nom : urlProduct.name,
+    image : urlProduct.imageUrl,
+    description : urlProduct.description,
+    prix : urlProduct.price,   
+};
+
+
+
+urlProduct = JSON.stringify(myProduct);
+
+localStorage.setItem('panier', urlProduct);
+
+
+console.log(localStorage.getItem('panier'));
+
+
+});
+
